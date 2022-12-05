74b515bd1bfd3cdc6b569c7e92936bbcc99e28c2
Heather Dice <heatherdice22@gmail.com>
2022-12-05 15:11:56 -0500

    Added and tested static files

--- lgbooktea/users/views.py
+++ lgbooktea/users/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, HttpResponse
 
 def index(request): # index needs request whether it's used or not
-    return HttpResponse("Hello Again, World")
+    test_data = {
+        'message' : 'hello'
+    }
+    return render(request, 'index.html', test_data)
